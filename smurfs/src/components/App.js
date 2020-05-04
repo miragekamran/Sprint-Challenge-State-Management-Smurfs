37a2350fd104407cd33457ae58a8f956b0e5adb6
mirage <mirage.kamran.pkt@gmail.com>
2020-05-03 18:04:01 -0700

    form and post request created

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,34 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { SmurfsContexts } from "../contexts/SmurfsContexts";
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
+import Smurfs from "./Smurfs";
+function App() {
+  const [smurfs, setSmurfs] = useState();
+
+  useEffect(() => {
+    axios
+      .get("ttp://localhost:3333/smurfs")
+      .then((res) => {
+        console.log(res.data);
+        setSmurfs(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  console.log(smurfs);
+
+  return (
+    <div className="App">
+      <h1>SMURFS! 2.0 W/ Redux</h1>
+      <div>Welcome to your state management version of Smurfs!</div>
+      <SmurfsContexts.Provider value={{smurfs}}>
+        <Smurfs />
+      </SmurfsContexts.Provider>
+    </div>
+  );
 }
 
 export default App;
---
+++ smurfs/src/components/Smurfs.js
@@ -0,0 +1,89 @@
+import React, { useContext, useState } from "react";
+import { SmurfsContexts } from "../contexts/SmurfsContexts";
+import axios from "axios";
+
+export default function Smurfs() {
+  const value = useContext(SmurfsContexts);
+
+  const [newSmurf, setNewSmurf] = useState({
+    name: "",
+    age: "",
+    height: "",
+  });
+
+  const handleChanges = (event) => {
+    setNewSmurf({
+      ...newSmurf,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3333/smurfs", newSmurf)
+      .then((res) => {
+        console.log(res);
+        value.smurfs.push("/");
+      })
+      .catch((err) => console.log(err))
+      .finally(() => window.location.reload());
+  };
+
+  function deleteSmurf(event, id) {
+    event.preventDefault();
+    axios
+      .delete(`http://localhost:3333/smurfs/${id}`)
+      .then((res) => console.log(res))
+      .catch((err) => console.log("delete", err))
+      .finally(() => window.location.reload());
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        Name: &nbsp;
+        <input
+          type="text"
+          name="name"
+          value={newSmurf.value}
+          onChange={handleChanges}
+        />
+        <br />
+        Age: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input
+          type="text"
+          name="age"
+          value={newSmurf.value}
+          onChange={handleChanges}
+        />
+        <br />
+        Height: {""};
+        <input
+          type="text"
+          name="height"
+          value={newSmurf.value}
+          onChange={handleChanges}
+        />
+        <br />
+        <button type="submit">Add Smurfs</button>
+      </form>
+      {value.smurfs
+        ? value.smurfs.map((smurf) => (
+            <div key={smurf.id}>
+              <p>
+                Name: {smurf.name}
+                <br />
+                Age: {smurf.age}
+                <br />
+                Hieght: {smurf.height}
+              </p>
+              <button onClick={(event) => deleteSmurf(event.smurf.id)}>
+                Delete Smurf
+              </button>
+            </div>
+          ))
+        : "Importing Smurfs"}
+    </div>
+  );
+}
